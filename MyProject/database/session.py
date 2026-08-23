54923d7d0dff65421bd6f12107edf9f19331dd9b
LRIacobescu <ligia_iacobescu@yahoo.com>
2026-08-24 00:34:13 +0300

    Remove MyProject from Git tracking

--- MyProject/database/session.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from config.settings import DATABASE_URL
-
-Base = declarative_base()
-
-engine = create_engine(DATABASE_URL, echo=False)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
